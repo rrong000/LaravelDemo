44c6320afb9efd4a23ef9253d1decfc51029d9ff
rrong <rrong0000@gmail.com>
2020-12-01 16:00:14 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InspiringController;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/hello-world', function () {
+
+    return view('hello_world');
+
+}); 
+Route::get('/about_us', function () {
+
+    return view('about_us', ['name' => 'Laravel 6.0 範例']);
+
+}); 
+Route::get('/inspire', 'App\Http\Controllers\InspiringController@inspire'); 
+
+Route::get('/test', function(){
+
+    return App\Models\Post::all();
+
+}); 
+Route::get('/edit', function(){
+
+    $post = App\Models\Post::find(1);
+
+    $post->content = 'Laravel demo';
+
+    $post->save();
+
+    return $post;
+
+}); 
+Route::get('/add', function(){
+
+    $post = new App\Models\Post;
+
+    $post->content = 'ABCDEF';
+
+    $post->save();
+
+    return $post;
+
+}); 
